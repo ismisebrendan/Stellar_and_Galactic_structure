eedea481fa3af3359b1ed91a8b574be7e00643c2
ismisebrendan <135332281+ismisebrendan@users.noreply.github.com>
2023-09-28 22:11:06 +0100

    Add files via upload

--- Stellar_CA1.py
+++ Stellar_CA1.py
@@ -27,7 +27,8 @@ plt.xlabel("$\lambda$ ($\AA$)")
 plt.ylabel("F$_{\lambda}$ (erg s$^{-1}$ cm$^{-2}$ $\AA^{-1}$)")
 plt.title("The flux (F$_{\lambda}$) per unit wavelength ($\lambda$) of a star \n with a stellar temperature of 6451 K")
 plt.legend()
-plt.savefig("C:/Users/brend/Documents/_University/3 JS/Stellar Structure/CA/blackbody.png")
+plt.savefig("/home/vboxuser/Documents/Stellar/blackbody.png")
+plt.close()
 
 ##################
 #                #
@@ -36,10 +37,10 @@ plt.savefig("C:/Users/brend/Documents/_University/3 JS/Stellar Structure/CA/blac
 ##################
 
 # Find index of maximum flux
-max_flux = np.where(fluxes == max(fluxes))[0]
+max_flux_loc = np.where(fluxes == max(fluxes))[0]
 
-# Wavelgength at this index
-peak_wavelength = wavelengths[max_flux-1].item() * 10**8 # convert to angstrom
+# Wavelength at this index
+peak_wavelength = wavelengths[max_flux_loc-1].item() * 10**8 # convert to angstrom
 
 print("The peak wavelength of the star's emission is " + str(peak_wavelength) + " \u00C5")
 
@@ -51,6 +52,11 @@ print("The peak wavelength of the star's emission is " + str(peak_wavelength) + 
 
 pc_fluxes = flux(wavelengths, 3100)
 
+# Find index of maximum flux for PC
+pc_max_flux_loc = np.where(pc_fluxes == max(pc_fluxes))[0]
+
+# Wavelength at this index for PC
+pc_peak_wavelength = wavelengths[pc_max_flux_loc-1].item() * 10**8 # convert to angstrom
 plt.plot(wavelengths, fluxes, label="'My star', T = 6451 K")
 plt.plot(wavelengths, pc_fluxes, label="Proxima Centauri, T = 3100 K")
 plt.xticks(np.linspace(0,0.0002,5), np.linspace(0,20000,5)) # Label like it's angstroms
@@ -58,16 +64,11 @@ plt.xlabel("$\lambda$ ($\AA$)")
 plt.ylabel("F$_{\lambda}$ (erg s$^{-1}$ cm$^{-2}$ $\AA^{-1}$)")
 plt.title("The flux (F$_{\lambda}$) per unit wavelength ($\lambda$) of a star \n with a stellar temperature of 6451 K")
 plt.legend()
-plt.savefig("C:/Users/brend/Documents/_University/3 JS/Stellar Structure/CA/blackbody_PC.png")
 
 
+plt.annotate("Peak of my\nstar's spectrum,\n$\lambda$ = " + str(np.round(peak_wavelength)) + " $\AA$", xy=(peak_wavelength*1e-8, max(fluxes)), xytext=(0.00004,0.4e15), arrowprops=dict(arrowstyle="->"))
+plt.annotate("Peak of Proxima\nCentauri's spectrum,\n$\lambda$ = " + str(np.round(pc_peak_wavelength)) + " $\AA$", xy=(pc_peak_wavelength*1e-8, max(pc_fluxes)), xytext=(0.0001,0.6e15), arrowprops=dict(arrowstyle="->"))
 
 
-
-
-
-
-
-
-
-
+plt.savefig("/home/vboxuser/Documents/Stellar/blackbody_PC.png")
+plt.close()
